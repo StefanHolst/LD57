fd7e4320d8a65d13b4604f21db495238ec7616fa
Jeppe <jeppe@j-software.dk>
2025-04-07 15:41:49 +0200

    Restructure menus

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -4,6 +4,10 @@ func Play():
 	# Starts the game
 	get_tree().change_scene_to_file("res://Game.tscn")
 	pass
+	
+func Highscore():
+	get_tree().change_scene_to_file("res://Menu/Highscore.tscn")
+	pass
 
 func Quit():
 	# Exits the game
